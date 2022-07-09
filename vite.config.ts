dadaea5a117f2402e1b93937b3fd7487b6e424db
hexagon06 <jasper.saaltink@gmail.com>
2022-07-09 12:29:48 +0200

    WIP added a bunch of missing peer dependencies without any change

--- vite.config.ts
+++ vite.config.ts
@@ -14,23 +14,23 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, './src/library.ts'),
-      name: 'vue3-boilerplate-library',
-      fileName: (format) => `vue3-boilerplate-library.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, './src/library.ts'),
+  //     name: 'vue3-boilerplate-library',
+  //     fileName: (format) => `vue3-boilerplate-library.${format}.js`,
+  //   },
+  //   rollupOptions: {
+  //     // make sure to externalize deps that shouldn't be bundled
+  //     // into your library
+  //     external: ['vue'],
+  //     output: {
+  //       // Provide global variables to use in the UMD build
+  //       // for externalized deps
+  //       globals: {
+  //         vue: 'Vue',
+  //       },
+  //     },
+  //   },
+  // },
 });
