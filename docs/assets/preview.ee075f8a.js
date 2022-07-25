9c5356acd9ec8cad261b2af51dd51cacc203afdf
hexagon06 <jasper.saaltink@gmail.com>
2022-07-25 19:37:41 +0200

    publish docs

---
+++ docs/assets/preview.ee075f8a.js
@@ -0,0 +1,2 @@
+/* empty css               */const r={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{r as parameters};
+//# sourceMappingURL=preview.ee075f8a.js.map
